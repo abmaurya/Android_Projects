47a42bffe5d2b123929948d61d7ceb1ace0ea14e
abmaurya <abmaurya@users.noreply.github.com>
2017-01-25 18:43:18 +0530

    Add files via upload

---
+++ CollegeAssignment/app/build/generated/source/buildConfig/debug/com/gamoblast/abm/collegeassignment/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.gamoblast.abm.collegeassignment;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.gamoblast.abm.collegeassignment";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
